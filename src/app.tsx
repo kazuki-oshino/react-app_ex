f3a8a50957eb442548d2c6035153dab3f7d53161
kazuikios <slash0juice@gmail.com>
2022-10-16 18:36:16 +0900

    feat: heading component

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,14 @@
+import {Text} from "./libs/Text"
+import {Heading} from "./libs/Heading";
+
 export const App = () => {
   return (
-    <h1>React App</h1>
+    <>
+      <Text text="Hello, World!" />
+      <Heading tag="h1">見出し</Heading>
+      <Heading tag="h2">
+        <span>hello, 日本!</span>
+      </Heading>
+    </>
   )
 }
